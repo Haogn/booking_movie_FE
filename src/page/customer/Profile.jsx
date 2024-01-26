2326c42d16912864c6817e3fb0d2baa02d064737
unknown <nshoang270298@gmail.com>
2024-01-26 09:40:07 +0700

    hoang

--- src/page/customer/Profile.jsx
+++ src/page/customer/Profile.jsx
@@ -1,27 +1,23 @@
 import React, { useEffect } from "react";
-import { useDispatch,useSelector  } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { profileUser } from "../../redux/api/service/customerRequest";
 
 function Profile() {
-<<<<<<< HEAD
-  const user = useSelector((state) => state.auth.login.currentUser);
-  console.log(user);
-=======
-  const storedToken  = localStorage.getItem('acessToken');
-  const token = storedToken && storedToken.startsWith('"') && storedToken.endsWith('"')
-  ? storedToken.slice(1, -1) 
-  : storedToken;
+  const storedToken = localStorage.getItem("acessToken");
+  const token =
+    storedToken && storedToken.startsWith('"') && storedToken.endsWith('"')
+      ? storedToken.slice(1, -1)
+      : storedToken;
   const dispatch = useDispatch();
-  const userProfile = useSelector(state => state.customer.profile.userProfile);
-  
->>>>>>> 519b089bedfe3f9c74fe1652ab2a5c0ab236ac09
+  const userProfile = useSelector(
+    (state) => state.customer.profile.userProfile
+  );
 
   useEffect(() => {
     profileUser(dispatch, token);
   }, [dispatch, token]);
 
-
-  return  userProfile ?  (
+  return userProfile ? (
     <div>
       <div className="bg-gray-900 text-white text-3xl font-medium font-mono text-center py-1">
         Thông tin tài khoản
@@ -35,20 +31,8 @@ function Profile() {
             alt="avatar"
           />
         </div>
-<<<<<<< HEAD
-        <input type="submit" id="avatarInput" className="hidden" />
+        <input type="file" id="avatarInput" className="hidden" />
         <button type="file" className="btn btn-dark font-mono mb-4 w-[116px]">
-=======
-        <input
-          type="file"
-          id="avatarInput"
-          className="hidden"
-        />
-        <button
-          type="file"
-          className="btn btn-dark font-mono mb-4 w-[116px]"
-        >
->>>>>>> 519b089bedfe3f9c74fe1652ab2a5c0ab236ac09
           Thay đổi
         </button>
       </div>
@@ -58,41 +42,34 @@ function Profile() {
           <div className="row">
             {/* Cột 1 */}
             <div className="col-md-6">
-            <div className="mb-3">
+              <div className="mb-3">
                 <label className="form-label font-mono font-semibold">
                   Tài khoản:<span className="text-red-500">*</span>
                 </label>
-<<<<<<< HEAD
-
-                <p className="font-mono font-semibold mt-2">{user.username}</p>
-=======
-                <p className="font-mono font-semibold mt-2">{userProfile.username}</p>
->>>>>>> 519b089bedfe3f9c74fe1652ab2a5c0ab236ac09
+                <p className="font-mono font-semibold mt-2">
+                  {userProfile.username}
+                </p>
               </div>
               <div className="mb-3">
                 <label className="form-label font-mono font-semibold">
                   Email: <span className="text-red-500">*</span>
                 </label>
-<<<<<<< HEAD
-                <p className="font-mono font-semibold mt-2">{user.email}</p>
-=======
                 <p className="font-mono font-semibold mt-2">
-                {userProfile.email}
+                  {userProfile.email}
                 </p>
->>>>>>> 519b089bedfe3f9c74fe1652ab2a5c0ab236ac09
               </div>
 
-              {/* <div className="mb-3">
+              <div className="mb-3">
                 <label className="form-label font-mono font-semibold">
-                  Ngày sinh: 
+                  Ngày sinh:
                 </label>
                 <p className="font-mono font-semibold mt-2">
-                {userProfile.dateOfBirth}
+                  {userProfile.dateOfBirth}
                 </p>
               </div>
               <div className="mb-3">
                 <label className="form-label font-mono font-semibold">
-                  Giới tính: 
+                  Giới tính:
                 </label>
                 <div className="flex gap-2">
                   <div className="form-check">
@@ -120,74 +97,63 @@ function Profile() {
                     </label>
                   </div>
                 </div>
-              </div> */}
+              </div>
             </div>
 
             {/* Cột 2 */}
             <div className="col-md-6">
               <div className="mb-3">
                 <label className="form-label font-mono font-semibold">
-                  Số điện thoại: 
+                  Số điện thoại:
                 </label>
                 <input
-<<<<<<< HEAD
+                  value={userProfile.phone}
                   type="text"
                   className="form-control"
                   id="phone"
-                  value={user.phone}
                 />
-=======
-                value={userProfile.phone}
-                 type="text" className="form-control" id="phone" />
->>>>>>> 519b089bedfe3f9c74fe1652ab2a5c0ab236ac09
               </div>
               <div className="mb-3">
                 <label className="form-label font-mono font-semibold">
-                  Thành phố: 
+                  Thành phố:
                 </label>
-<<<<<<< HEAD
-                <p className="font-mono font-semibold mt-2">
-                  {user.dateOfBirth}
-                </p>
-=======
-                <input 
-                value={userProfile.city}
-                type="text" className="form-control" id="city" />
->>>>>>> 519b089bedfe3f9c74fe1652ab2a5c0ab236ac09
+                <input
+                  value={userProfile.city}
+                  type="text"
+                  className="form-control"
+                  id="city"
+                />
               </div>
               <div className="mb-3">
                 <label className="form-label font-mono font-semibold">
                   Địa chỉ:
                 </label>
                 <input
-                value={userProfile.address}
-                 type="text" className="form-control" id="address" />
+                  value={userProfile.address}
+                  type="text"
+                  className="form-control"
+                  id="address"
+                />
               </div>
-             
+
               <div className="mb-3">
                 <label className="form-label font-mono font-semibold">
                   Hạng thành viên:<span className="text-red-500">*</span>
                 </label>
-                <p className="font-mono font-semibold mt-2">{userProfile.level}</p>
+                <p className="font-mono font-semibold mt-2">
+                  {userProfile.level}
+                </p>
               </div>
             </div>
           </div>
-<<<<<<< HEAD
-          <button
-            type="sunmit"
-            className="btn btn-dark font-mono mb-4 text-white"
-          >
-=======
           <button type="file" className="btn btn-dark font-mono mb-4">
->>>>>>> 519b089bedfe3f9c74fe1652ab2a5c0ab236ac09
             Lưu thay đổi
           </button>
         </form>
       </div>
     </div>
-  ):(
-    <>
-    </>
+  ) : (
+    <></>
   );
 }
 
--- src/page/login_register/Login.jsx
+++ src/page/login_register/Login.jsx
@@ -15,14 +15,13 @@ function Login() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
- const error = useSelector((state)=>state.auth.login.error)
+  const error = useSelector((state) => state.auth.login.error);
 
-
- useEffect(()=>{
-  return () => {
-    dispatch(resetError());
-  };
- },[dispatch])
+  useEffect(() => {
+    return () => {
+      dispatch(resetError());
+    };
+  }, [dispatch]);
 
   // useEffect(() => {
   //   handleGetCaptcha();
@@ -50,12 +49,12 @@ function Login() {
     setErrorUsername(null);
     setErrorPassword(null);
     if (validateBlank(username)) {
-      setErrorUsername('Tài khoản không được để trống.');
-      return ;
+      setErrorUsername("Tài khoản không được để trống.");
+      return;
     }
     if (validateBlank(password)) {
-      setErrorPassword('Mật khẩu không được để trống.');
-      return ;
+      setErrorPassword("Mật khẩu không được để trống.");
+      return;
     }
     dispatch(resetError());
     const user = {
@@ -73,36 +72,40 @@ function Login() {
             Đăng nhập
           </h1>
           <div className="w-[80%] h-full mx-auto">
-            {error?(<span className="text-red-500">{error.data}</span>):(<></>)}
+            {error ? <span className="text-red-500">{error.data}</span> : <></>}
             <form action="" onSubmit={handleLogin}>
-            <div className="mb-3">
-        <label className="form-label font-mono font-semibold">
-          Tên đăng nhập: <span className="text-red-400">*</span>
-        </label>
-        <input
-          onChange={(e) => setUsername(e.target.value)}
-          type="text"
-          className="form-control"
-          id="username"
-          name="username"
-        />
-        {errorUsername && <span className="text-red-500">{errorUsername}</span>}
-      </div>
+              <div className="mb-3">
+                <label className="form-label font-mono font-semibold">
+                  Tên đăng nhập: <span className="text-red-400">*</span>
+                </label>
+                <input
+                  onChange={(e) => setUsername(e.target.value)}
+                  type="text"
+                  className="form-control"
+                  id="username"
+                  name="username"
+                />
+                {errorUsername && (
+                  <span className="text-red-500">{errorUsername}</span>
+                )}
+              </div>
 
-      <div className="mb-3">
-        <label className="form-label font-mono font-semibold">
-          Mật khẩu: <span className="text-red-400">*</span>
-        </label>
-        <input
-          onChange={(e) => setPassword(e.target.value)}
-          type="password"
-          className="form-control"
-          id="password"
-          name="password"
-          autoComplete="password"
-        />
-        {errorPassword && <span className="text-red-500">{errorPassword}</span>}
-      </div>
+              <div className="mb-3">
+                <label className="form-label font-mono font-semibold">
+                  Mật khẩu: <span className="text-red-400">*</span>
+                </label>
+                <input
+                  onChange={(e) => setPassword(e.target.value)}
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  name="password"
+                  autoComplete="password"
+                />
+                {errorPassword && (
+                  <span className="text-red-500">{errorPassword}</span>
+                )}
+              </div>
               <div className="flex justify-between">
                 {/* <div className="mb-3">
                   <label className="form-label font-mono font-semibold">
